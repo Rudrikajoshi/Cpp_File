5668b1b8f126b867b0f870ceed5581970076bf73
Rudrikajoshi <142895294+Rudrikajoshi@users.noreply.github.com>
2025-02-27 23:43:22 +0530

    Add files via upload

---
+++ intToRoman.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string intToRoman(int num)
+{
+    vector<pair<int, string>> roman = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, 
+        {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, 
+        {5, "V"}, {4, "IV"}, {1, "I"}
+    };
+
+    string result = "";
+
+    for (auto &pair : roman) {
+        while (num >= pair.first) {
+            result += pair.second; 
+            num -= pair.first;     
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    string romanNumeral = intToRoman(num);
+    cout << "Roman Numeral: " << romanNumeral << endl;
+
+    return 0;
+}
